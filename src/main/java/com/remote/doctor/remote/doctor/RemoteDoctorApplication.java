f28cfab17f4ae22dbe405284c91441ac4ae070d9
Volodymyr <volodymyr.maiboroda@nure.ua>
2018-10-23 12:34:06 +0300

    initial spring boot project

---
+++ src/main/java/com/remote/doctor/remote/doctor/RemoteDoctorApplication.java
@@ -0,0 +1,12 @@
+package com.remote.doctor.remote.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteDoctorApplication.class, args);
+	}
+}
